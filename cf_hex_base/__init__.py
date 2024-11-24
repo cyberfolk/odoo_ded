8193dc8d404ccdd0df4884ad62d33b75fa19784d
Andrea Ferrari <114855536+cyberfolk@users.noreply.github.com>
2024-11-24 10:29:38 +0000

    [UPD] set viewClient as first page

--- cf_hex_base/__init__.py
+++ cf_hex_base/__init__.py
@@ -4,5 +4,5 @@ from .utility.constant import BORDERS_MAP
 
 
 def post_init_hook_cf_hex_base(env):
-    env["hex.macro"].create([{"name": "Mappa V1 - 01", "type": "v1_19_q"}])
     env["hex.macro"].create([{"name": "Mappa V2 - 01", "type": "v2_nolimit_q"}])
+    env["hex.macro"].create([{"name": "Mappa V1 - 01", "type": "v1_19_q"}])
