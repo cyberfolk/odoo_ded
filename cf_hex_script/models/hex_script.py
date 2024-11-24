6b7dbbd1f8ff4af9bb23351437175e20b83506de
Andrea Ferrari <114855536+cyberfolk@users.noreply.github.com>
2024-11-24 11:13:12 +0100

    [UPD] one2one field hex_script

--- cf_hex_script/models/hex_script.py
+++ cf_hex_script/models/hex_script.py
@@ -1,5 +1,5 @@
 import json
-from odoo import api, fields, models, Command
+from odoo import api, fields, models, Command, api
 
 
 class HexScript(models.Model):
@@ -18,14 +18,30 @@ class HexScript(models.Model):
         help="Bioma contenuto in questo Hex-Script"
     )
 
-    hex_id = fields.Many2one(
-        comodel_name='hex.hex',
-        string="Esagono",
-        help="Esagono relativo a questo Hex-Script"
-    )
-
     sml = fields.Integer(
         string="SML",
         help="Difficoltà Hex-Script. Calcolata come 'Scontro Mortale per 4 PG di Livello SML'"
     )
 
+    hex_id = fields.Many2one(
+        comodel_name='hex.hex',
+        compute='compute_hex',
+        inverse='hex_inverse',
+        string="Esagono",
+    )
+
+    hex_ids = fields.One2many(
+        comodel_name='hex.hex',
+        inverse_name='hex_script_id'
+    )
+
+    @api.depends('hex_ids')
+    def compute_hex(self):
+        if len(self.hex_ids) > 0:
+            self.hex_id = self.hex_ids[0]
+
+    def hex_inverse(self):
+        if len(self.hex_ids) > 0: # delete previous reference
+            hex = self.env['hex.hex'].browse(self.hex_ids[0].id)
+            hex.hex_id = False
+        self.hex_id.hex_script_id = self  # set new reference
