ac5a3092131f4db412283e5d957dcc2adeba9b7c
cyberfolk <andrea1995ferrari@gmail.com>
2024-12-26 11:17:31 +0100

    [UPD] data.py

--- cf_hex_data/data/hex_hex.py
+++ cf_hex_data/data/hex_hex.py
@@ -1,6 +1,1 @@
-dicts = [
-    {
-        'name': 'void', 'status': 'script', 'sml': 1, 'description': '', 'image': '', 'structure_id': False,
-        'creature_id': False, 'biome_id': False, 'npc_ids': [], 'faction_ids': []
-    }
-]
+dicts = [{'name': 'void', 'status': 'script', 'sml': 1, 'description': '', 'image': '', 'structure_id': False, 'creature_id': False, 'biome_id': False, 'npc_ids': [], 'faction_ids': []}]
